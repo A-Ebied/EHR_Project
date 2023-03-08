3610949a5d87347c5da0d81003340f109f59a92d
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2023-03-08 22:13:11 +0200

    modify of  AllergyAPI

--- EHR_API/Controllers/AllergyAPIController.cs
+++ EHR_API/Controllers/AllergyAPIController.cs
@@ -26,25 +26,25 @@ namespace EHR_API.Controllers
 
 
         //[Authorize]
-        [HttpGet("{id}")]
+        [HttpGet("GetUserAllergies")]
         [ResponseCache(CacheProfileName = SD.ProfileName)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<APIResponse>> GetUserAllergies(string id)
+        public async Task<ActionResult<APIResponse>> GetUserAllergies(string userId)
         {
             try
             {
-                if (id == null)
+                if (userId == null)
                 {
                     return BadRequest(APIResponses.BadRequest("Id is null"));
                 }
 
                 var entities = await _db._allergy.GetAllAsync(
-                    expression: g => g.RegistrationDataId == id);
+                    expression: g => g.RegistrationDataId == userId);
                 if (entities == null)
                 {
-                    return BadRequest(APIResponses.BadRequest($"No objects with Id = {id} "));
+                    return BadRequest(APIResponses.BadRequest($"No objects with Id = {userId} "));
                 }
 
                 _response.Result = _mapper.Map<List<AllergyDTOForOthers>>(entities);
