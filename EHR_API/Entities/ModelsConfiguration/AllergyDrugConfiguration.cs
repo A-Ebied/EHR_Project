b47b45c799b77230538f5d77694c6454f33dce4c
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2023-03-14 15:58:41 +0200

    AllergyDrugConfiguration

---
+++ EHR_API/Entities/ModelsConfiguration/AllergyDrugConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EHR_API.Entities.Models;
+
+namespace EHR_API.Entities.ModelsConfiguration
+{
+    public class AllergyDrugConfiguration : IEntityTypeConfiguration<AllergyDrug>
+    {
+        public void Configure(EntityTypeBuilder<AllergyDrug> builder)
+        {
+            builder.HasData(
+                new AllergyDrug
+                {
+                    AllergyId = 1,
+                    MedicationId = 1,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                },
+                new AllergyDrug
+                {
+                    AllergyId = 1,
+                    MedicationId = 2,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                });
+        }
+    }
+}
